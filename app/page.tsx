ef1827cb0c4403303542efc3c3ad80a8c4a0e6dd
JimmyGithub007 <jimmygithub007@gmail.com>
2024-05-08 22:13:08 +0800

    Design v1 - 6

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { LuMouse } from "react-icons/lu";
 import { About, Contact, Header, Menu, Panel, ScrollDot, Service, Work } from "@/components";
 import { RootState } from "@/store";
-import { minusFloor, plusFloor } from "@/store/slice/floorSlice";
+import { minusFloor, plusFloor, setFloor } from "@/store/slice/floorSlice";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
   const { floor } = useSelector((state: RootState) => state.floor);
+
 
   useEffect(() => {
     const element = document.getElementById(`floor${floor}`);
@@ -20,10 +22,10 @@ const Home = () => {
 
   useEffect(() => {
     const handleScroll = (event: WheelEvent) => {
-      if(event.deltaY > 0) {
+      if (event.deltaY > 0) {
         dispatch(plusFloor());
       }
-      if(event.deltaY < 0) {
+      if (event.deltaY < 0) {
         dispatch(minusFloor());
       }
     };
@@ -45,10 +47,10 @@ const Home = () => {
       <Service />
       <Work />
       <Contact />
-      { floor !== 4 && <div className="font-bold fixed flex flex-col items-center justify-center bottom-[1%] w-full">
+      {floor !== 4 && <motion.div className="font-bold fixed flex flex-col items-center justify-center bottom-[1%] w-full">
         <LuMouse className="animate-bounce text-blue-950 text-4xl z-10" />
         Scroll
-      </div> }
+      </motion.div>}
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,52 @@
 import { useDispatch, useSelector } from "react-redux";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { motion } from "framer-motion";;
-import { setIsOpen } from "@/store/slice/floorSlice";
+import { setFloor, setIsOpen } from "@/store/slice/floorSlice";
+import { RootState } from "@/store";
 
 import Image from "next/image"
-import { RootState } from "@/store";
 
 const Header = () => {
     const dispatch = useDispatch();
     const { floor } = useSelector((state: RootState) => state.floor);
 
     return (<div className={`bg-white duration-300 fixed top-0 w-full z-10 ${floor > 0 ? "h-16" : "h-28"}`}>
-        <div className="duration-300 flex h-full items-center justify-between px-4">
+        <div className="duration-300 flex h-full items-center justify-between px-8">
             <div className={`duration-300 overflow-hidden ${floor > 0 ? "h-[50px]" : "h-[100px]"}`}>
                 <Image className={`duration-300 rounded-md ${floor > 0 ? "-translate-y-[25px] w-[100px]" : "-translate-y-[50px] w-[200px]"}`} alt="logo" width={200} height={200} src={"/assets/logo.jpg"} />
             </div>
-            <motion.button 
-                whileHover={{ scale: 1.2, rotate: 360 }} 
+            <motion.button
+                whileHover={{ scale: 1.2, rotate: 360 }}
                 whileTap={{
                     scale: 0.8,
                     rotate: -90,
                     borderRadius: "100%"
                 }}
-                onClick={() => dispatch(setIsOpen(true)) }
+                onClick={() => dispatch(setIsOpen(true))}
                 className="bg-slate-100 p-2 rounded-xl shadow-sm text-2xl md:hidden">
                 <HiMenuAlt3 />
             </motion.button>
+            <div className="font-bold gap-4 hidden md:flex">
+                {
+                    ["主页", "关于我们", "我们提供的服务", "成品展示", "联系我们"].map((value, key) => (
+                        <motion.button 
+                            initial={{
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1,
+                                transition: {
+                                    duration: 0.5,
+                                    delay: key*0.1
+                                }
+                            }}
+                            key={key} onClick={() => { dispatch(setFloor(key)) }} 
+                            className={`duration-200 hover:text-slate-300 ${key === floor ? "text-slate-400" : "text-blue-950"}`}>
+                            {value}
+                        </motion.button>
+                    ))
+                }
+            </div>
         </div>
     </div>)
 }
